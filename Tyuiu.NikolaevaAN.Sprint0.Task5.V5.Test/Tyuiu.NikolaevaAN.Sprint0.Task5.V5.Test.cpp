86aa6fbceae54b92f3ac458111794293d2a7a49f
Tyuiu.NikolaevaAN@outlook.com <Tyuiu.NikolaevaAN@outlook.com>
2023-09-12 17:37:01 +0500

    Task5

---
+++ Tyuiu.NikolaevaAN.Sprint0.Task5.V5.Test/Tyuiu.NikolaevaAN.Sprint0.Task5.V5.Test.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.NikolaevaAN.Sprint0.Task5.V5.Lib/Tyuiu.NikolaevaAN.Sprint0.Task5.V5.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuiuNikolaevaANSprint0Task5V5Test
+{
+	TEST_CLASS(TyuiuNikolaevaANSprint0Task5V5Test)
+	{
+	public:
+		TEST_METHOD(TestMethod5)
+		{
+			// Init
+			ISprint0Task5* date = new ISprint0Task5();
+			int a = 4;
+			int b = 5;
+			int c = 6;
+			int d;
+
+			// run
+			d = date->Add(a, b, c);
+
+			// Valid
+			Assert::AreEqual(25, d);
+		}
+	};
+}
