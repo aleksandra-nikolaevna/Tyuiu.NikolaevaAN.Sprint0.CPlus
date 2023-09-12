d7d885b82fe9ee8a16511c83a70c3b18d74275cb
Tyuiu.NikolaevaAN@outlook.com <Tyuiu.NikolaevaAN@outlook.com>
2023-09-12 17:28:09 +0500

    Task5

---
+++ Tyuiu.NikolaevaAN.Sprint0.Task5.V5.Lib/Tyuiu.NikolaevaAN.Sprint0.Task5.V5.Lib.cpp
@@ -0,0 +1,15 @@
+﻿// Tyuiu.NikolaevaAN.Sprint0.Task5.V5.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: This is an example of a library function
+class ISprint0Task5
+{
+public:
+	virtual int Add(int a, int b, int c)
+	{
+		return (a + b + c) + (a * b / 2);
+	}
+};
